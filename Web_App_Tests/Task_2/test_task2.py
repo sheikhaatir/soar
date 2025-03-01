299851f85449a95a0afb7a7ce20f102f1d9b7b1e
Sheikh Aatir <sheikhaatir@gmail.com>
2025-03-01 23:34:36 +0500

    Modified Task 2

--- Web_App_Tests/Task_2/test_task2.py
+++ Web_App_Tests/Task_2/test_task2.py
@@ -15,15 +15,43 @@ class TestWebApp:
         try:
             welcome_close = driver.find_element(By.CSS_SELECTOR, "#mat-dialog-0 button.close-dialog")
             welcome_close.click()
-        except Exception as e:
-            print(f"Welcome banner not found or already closed: {e}")
-
-        try:
             cookie_close = driver.find_element(By.CSS_SELECTOR, "body > div.cc-window.cc-floating.cc-type-info.cc-theme-classic.cc-bottom.cc-right.cc-color-override--1225450786 > div > a")
             cookie_close.click()
         except Exception as e:
-            print(f"Cookie consent not found or already closed: {e}")
-
+            print(f"Cookie/Welcome consent not found or already closed: {e}")
+            
+        try: 
+            first_product = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/app-root/div/mat-sidenav-container/mat-sidenav-content/app-search-result/div/div/div[2]/mat-grid-list/div/mat-grid-tile[1]/div/mat-card')))
+            first_product.click()
+            print("✅ First product clicked")
+            
+            driver.save_screenshot("after_click.png")
+            print("📸 Screenshot saved: after_click.png")
+            time.sleep(10)
+            
+            product_popup = driver.find_elements(By.CSS_SELECTOR, "mat-dialog-container")
+            if driver.find_elements(By.CSS_SELECTOR, "mat-dialog-container"):
+                print("✅ Popup is displayed")
+            else:
+                print("❌ Popup is not displayed")
+            assert product_popup[0].is_displayed(), "Popup did not appear after clicking on 'Apple Juice'."
+            
+            time.sleep(5)
+            expand_review = driver.find_element(By.CSS_SELECTOR, "#mat-expansion-panel-header-0")
+            time.sleep(5)
+            expand_review.click()
+            print("✅ Review expanded")
+            time.sleep(5)
+            wait = WebDriverWait(driver, 10)
+            
+            close_button = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "button.mat-focus-indicator.close-dialog.buttons.mat-stroked-button.mat-button-base")))
+            driver.execute_script("arguments[0].scrollIntoView({block: 'center', inline: 'nearest'});", close_button)
+            time.sleep(5)
+            close_button.click()
+            print("✅ Popup closed")
+            time.sleep(5)
+        except Exception as e:
+            pytest.fail(f"❌ Test Failed: {e}")
 
 if __name__ == "__main__":
     pytest.main(["-v", __file__])
